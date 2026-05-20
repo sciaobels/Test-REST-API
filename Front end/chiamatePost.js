7ac24078d80d451227b18628c08e4cbe8f003975
Angelo <st10934@iismarconipieralisi.it>
2026-05-20 16:47:09 +0200

    Add files via upload
    Tolte console.log di debug

--- Front end/chiamatePost.js
+++ Front end/chiamatePost.js
@@ -1,19 +1,18 @@
 document.getElementById("inserimento").addEventListener("submit", postScusa);
 
 async function postScusa(e){
-  console.log("1");
+
   e.preventDefault();
   let scusa = document.getElementById("scusa").value;
   let tipo = document.getElementById("tipoScusa").value;
 
-  console.log(scusa);
-  console.log(tipo);
+
 
   try{
     let response;
     switch(tipo){
       case "compiti":
-        console.log("2")
+
         esponse = await fetch("http://localhost/src/API/index.php/postCompiti", {
           method: "POST",
           headers: {
